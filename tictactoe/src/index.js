c39e797ec7404b3141eb5e384e853ca463eac3f3
hyeomjeong <hyeom1027@gmail.com>
2021-03-16 20:12:25 +0900

    first commit

---
+++ tictactoe/src/index.js
@@ -0,0 +1,10 @@
+
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './Game';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<Game />,
+    document.getElementById('root')
+  );
+reportWebVitals();
